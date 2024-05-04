c1da7bced10ed51b94b1bb88ea1dfa50b2b90aeb
Heiko Panjas <heiko@panjas.com>
2024-05-05 00:10:36 +0200

    feat: Add files

---
+++ ADM/ADM.swift
@@ -0,0 +1,186 @@
+//MIT License
+//
+//Copyright (c) 2024 ultraschall.fm
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+import ArgumentParser
+import AVFoundation
+
+@main
+struct ADM: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "A utility for managing audio devices.",
+        version: "1.0.0-preview1",
+        subcommands: [List.self, Defaults.self],
+        defaultSubcommand: List.self)
+
+    mutating func run() throws {
+    }
+
+    static func printDevices(audioDevices: [AudioDevice]) {
+        let padding = audioDevices.max(by: {
+            if let lhs = $0.name, let rhs = $1.name {
+                return rhs.count > lhs.count
+            }
+            return false
+        })?.name?.count ?? 0
+
+        for audioDevice in audioDevices {
+            if let name = audioDevice.name, let uid = audioDevice.uid {
+                print(String(format: "   %3d: \(name.padding(toLength: padding, withPad: " ", startingAt: 0)) [uid = \(uid)]", audioDevice.id))
+            }
+        }
+    }
+}
+
+extension ADM {
+    struct List: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Print available audio devices.")
+
+        struct Options: ParsableArguments {
+            @Flag(name: [.long, .customShort("o")], help: "Print output audio devices.")
+            var output = false
+
+            @Flag(name: [.long, .customShort("i")], help: "Print input audio devices.")
+            var input = false
+        }
+        @OptionGroup var options: Options
+
+
+        mutating func run() {
+            if options.output == false && options.input == false {
+                options.output = true
+                options.input = true
+            }
+
+            let audioDevices = AudioDeviceManager.findDevices()
+            if options.output == true {
+                print("Output devices:")
+                printDevices(audioDevices: audioDevices.filter({$0.hasOutput == true }))
+            }
+            if options.input == true {
+                print("Input devices:")
+                printDevices(audioDevices: audioDevices.filter({$0.hasInput == true }))
+            }
+        }
+    }
+
+    struct Defaults: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Set or get default input and output devices.",
+                                                        subcommands: [Get.self, Set.self])
+
+        struct Options: ParsableArguments {
+            @Flag(name: [.long, .customShort("i")], help: "Set or get default input audio device.")
+            var input = false
+
+            @Flag(name: [.long, .customShort("o")], help: "Set or get default output audio device.")
+            var output = false
+        }
+        @OptionGroup var options: Options
+
+        mutating func run() {
+        }
+
+        struct Get: ParsableCommand {
+            static let configuration = CommandConfiguration(abstract: "Get default input or output device.")
+
+            @OptionGroup var options: Defaults.Options
+
+            mutating func run() {
+                if options.output == false && options.input == false {
+                    options.output = true
+                    options.input = true
+                }
+
+                let audioDevices = AudioDeviceManager.findDevices()
+                if options.output == true {
+                    print("Default output device:")
+                    printDevices(audioDevices: audioDevices.filter({$0.defaultOutput == true }))
+                }
+                if options.input == true {
+                    print("Default input device:")
+                    printDevices(audioDevices: audioDevices.filter({$0.defaultInput == true }))
+                }
+            }
+        }
+
+        struct Set: ParsableCommand {
+            static let configuration = CommandConfiguration(abstract: "Set default input or output device.")
+
+            struct Options: ParsableArguments {
+                @Flag(name: [.long, .customShort("n")], help: "Specify audio device name instead of audio device id.")
+                var name = false
+
+                @Flag(name: [.long, .customShort("u")], help: "Specify audio device uid instead of audio device id.")
+                var uid = false
+
+                @Argument(help: "An audio device id, name or uid to operate on.")
+                var value: String
+            }
+
+            @OptionGroup var options: Options
+            @OptionGroup var superOptions: Defaults.Options
+
+            mutating func run() {
+                if superOptions.input == true && superOptions.output == true {
+                }
+                else if superOptions.input == false && superOptions.output == false {
+                }
+                else if options.name == true && options.uid == true {
+                }
+                else {
+                    let audioDevices = AudioDeviceManager.findDevices()
+                    if options.name == true {
+                        if let audioDevice = audioDevices.first(where: {$0.name?.lowercased() == options.value.lowercased()}) {
+                            if superOptions.input {
+                                audioDevice.defaultInput = true
+                            }
+                            else {
+                                audioDevice.defaultOutput = true
+                            }
+                        }
+                    }
+                    else if options.uid == true {
+                        if let audioDevice = audioDevices.first(where: {$0.uid?.lowercased() == options.value.lowercased()}) {
+                            if superOptions.input {
+                                audioDevice.defaultInput = true
+                            }
+                            else {
+                                audioDevice.defaultOutput = true
+                            }
+                        }
+                    }
+                    else {
+                        if let deviceId = UInt32(options.value) as AudioDeviceID? {
+                            if let audioDevice = audioDevices.first(where: {$0.id == deviceId}) {
+                                if superOptions.input {
+                                    audioDevice.defaultInput = true
+                                }
+                                else {
+                                    audioDevice.defaultOutput = true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ADM/AudioDevice.swift
@@ -0,0 +1,225 @@
+//MIT License
+//
+//Copyright (c) 2024 ultraschall.fm
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+import AVFoundation
+
+class AudioDevice {
+    var id: AudioDeviceID
+
+    init(from: AudioDeviceID) {
+        self.id = from
+    }
+
+    var hasOutput: Bool {
+        get {
+            var hasOutput = false
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioDevicePropertyStreamConfiguration,
+                mScope: kAudioDevicePropertyScopeOutput,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize = UInt32(MemoryLayout<AudioBufferList?>.size);
+            var status = AudioObjectGetPropertyDataSize(self.id, &address, 0, nil, &dataSize);
+            if (status == 0) {
+                let data = UnsafeMutablePointer<AudioBufferList>.allocate(capacity: Int(dataSize))
+                status = AudioObjectGetPropertyData(self.id, &address, 0, nil, &dataSize, data);
+                if (status == 0) {
+                    let buffers = UnsafeMutableAudioBufferListPointer(data)
+                    for i in 0..<buffers.count {
+                        if buffers[i].mNumberChannels > 0 {
+                            hasOutput = true
+                            break
+                        }
+                    }
+                }
+                data.deallocate()
+            }
+
+            return hasOutput
+        }
+    }
+
+    var hasInput: Bool {
+        get {
+            var hasInput = false
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioDevicePropertyStreamConfiguration,
+                mScope: kAudioDevicePropertyScopeInput,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize = UInt32(MemoryLayout<AudioBufferList?>.size);
+            var status = AudioObjectGetPropertyDataSize(self.id, &address, 0, nil, &dataSize);
+            if (status == 0) {
+                let data = UnsafeMutablePointer<AudioBufferList>.allocate(capacity: Int(dataSize))
+                status = AudioObjectGetPropertyData(self.id, &address, 0, nil, &dataSize, data);
+                if (status == 0) {
+                    let buffers = UnsafeMutableAudioBufferListPointer(data)
+                    for i in 0..<buffers.count {
+                        if buffers[i].mNumberChannels > 0 {
+                            hasInput = true
+                            break
+                        }
+                    }
+                }
+                data.deallocate()
+            }
+
+            return hasInput
+        }
+    }
+
+    var uid: String? {
+        get {
+            var uid: CFString? = nil;
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioDevicePropertyDeviceUID, // CFString
+                mScope: kAudioObjectPropertyScopeGlobal,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize = UInt32(MemoryLayout<CFString?>.size)
+            var status = AudioObjectGetPropertyDataSize(self.id, &address, 0, nil, &dataSize)
+            if(status == 0) {
+                let data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<CFString>.alignment)
+                status = AudioObjectGetPropertyData(self.id, &address, 0, nil, &dataSize, data)
+                if (status == 0) {
+                    uid = data.load(as: CFString.self)
+                }
+                data.deallocate()
+            }
+
+            return uid as String?
+        }
+    }
+
+    var name: String? {
+        get {
+            var name: CFString? = nil
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioDevicePropertyDeviceNameCFString,
+                mScope: kAudioObjectPropertyScopeGlobal,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize = UInt32(MemoryLayout<CFString?>.size)
+            var status = AudioObjectGetPropertyDataSize(self.id, &address, 0, nil, &dataSize)
+            if status == 0 {
+                let data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<CFString>.alignment)
+                status = AudioObjectGetPropertyData(self.id, &address, 0, nil, &dataSize, data)
+                if status == 0 {
+                    name = data.load(as: CFString.self)
+                }
+                data.deallocate()
+            }
+
+            return name as String?
+        }
+    }
+
+    var defaultOutput: Bool {
+        get {
+            var isDefault = false
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioHardwarePropertyDefaultOutputDevice,
+                mScope: kAudioObjectPropertyScopeGlobal,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize: UInt32 = 0
+            var status = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize)
+            if status == 0 {
+                var deviceId: UInt32 = 0
+                status = AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize, &deviceId)
+                if status == 0 {
+                    if deviceId == self.id {
+                        isDefault = true
+                    }
+                }
+            }
+
+            return isDefault
+        }
+
+        set {
+            if (newValue == true) && self.hasOutput {
+                var address = AudioObjectPropertyAddress(
+                    mSelector: kAudioHardwarePropertyDefaultOutputDevice,
+                    mScope: kAudioObjectPropertyScopeGlobal,
+                    mElement: kAudioObjectPropertyElementMain)
+
+                var dataSize: UInt32 = 0
+                var status = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize)
+                if status == 0 {
+                    var deviceId = self.id
+                    status = AudioObjectSetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, dataSize, &deviceId)
+                    if status == 0 {
+                    }
+                }
+            }
+        }
+    }
+
+    var defaultInput: Bool {
+        get {
+            var isDefault = false
+
+            var address = AudioObjectPropertyAddress(
+                mSelector: kAudioHardwarePropertyDefaultInputDevice,
+                mScope: kAudioObjectPropertyScopeGlobal,
+                mElement: kAudioObjectPropertyElementMain)
+
+            var dataSize: UInt32 = 0
+            var status = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize)
+            if status == 0 {
+                var deviceId: UInt32 = 0
+                status = AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize, &deviceId)
+                if status == 0 {
+                    if deviceId == self.id {
+                        isDefault = true
+                    }
+                }
+            }
+
+            return isDefault
+        }
+
+        set {
+            if (newValue == true) && self.hasInput {
+                var address = AudioObjectPropertyAddress(
+                    mSelector: kAudioHardwarePropertyDefaultInputDevice,
+                    mScope: kAudioObjectPropertyScopeGlobal,
+                    mElement: kAudioObjectPropertyElementMain)
+
+                var dataSize: UInt32 = 0
+                var status = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &dataSize)
+                if status == 0 {
+                    var deviceId = self.id
+                    status = AudioObjectSetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, dataSize, &deviceId)
+                    if status == 0 {
+                    }
+                }
+            }
+        }
+    }
+}
